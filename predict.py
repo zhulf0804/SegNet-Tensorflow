72c7b7b3ea134d29012abeb6937d9afe4566248e
zhulf <zhulf0804@gmail.com>
2019-03-25 12:48:17 +0800

    add color prediction

--- predict.py
+++ predict.py
@@ -10,13 +10,39 @@ import matplotlib.pyplot as plt
 import segnet as Seg
 import input_data
 
-BATCH_SIZE = 4
+BATCH_SIZE = 1
 HEIGHT = 360
 WIDTH = 480
 saved_ckpt_path = './checkpoint/'
+saved_prediction = './pred/'
+
+classes = ['Sky', 'Building', 'Pole', 'Road', 'Pavement', 'Tree', 'SignSymbol', 'Fence', 'Car', 'Pedestrian', 'Bicyclist']
+cmap = np.array([[0, 0, 0],
+                [128, 128, 128],
+                [128, 0, 0],
+                [192, 192, 192],
+                [128, 64, 128],
+                [60, 40, 222],
+                [128, 128, 0],
+                [192, 128, 128],
+                [64, 64, 128],
+                [64, 0, 128],
+                [64, 64, 0],
+                [0, 128, 192]
+                ])
+
+def color_gray(image):
+    height, width = image.shape
+
+    return_img = np.zeros([height, width, 3], np.uint8)
+    for i in range(height):
+        for j in range(width):
+            return_img[i, j, :] = cmap[image[i, j]]
+
+    return return_img
 
 
-image_batch, anno_batch, filename = input_data.read_batch(BATCH_SIZE)
+image_batch, anno_batch, filename = input_data.read_batch(BATCH_SIZE, type='val')
 
 with tf.name_scope("input"):
 
@@ -43,10 +69,32 @@ with tf.Session() as sess:
         pred = sess.run(prediction, feed_dict={x: b_image})
 
 
-        # visualization
+        # save raw image, annotation, and prediction
         pred = pred.astype(np.uint8)
+        b_anno = b_anno.astype(np.uint8)
+        pred_color = color_gray(pred[0, :, :])
+        b_anno_color = color_gray(b_anno[0, :, :])
+
         b_image *= 255
         b_image = b_image.astype(np.uint8)
+
+        img = Image.fromarray(b_image[0])
+        anno = Image.fromarray(b_anno_color)
+        pred = Image.fromarray(pred_color)
+
+        basename = b_filename[0].split('.')[0]
+        #print(basename)
+
+        if not os.path.exists(saved_prediction):
+            os.mkdir(saved_prediction)
+        img.save(os.path.join(saved_prediction, basename + '.png'))
+        anno.save(os.path.join(saved_prediction, basename + '_anno.png'))
+        pred.save(os.path.join(saved_prediction, basename + '_pred.png'))
+
+        print("%s.png: prediction saved in %s" % (basename, saved_prediction))
+
+
+        '''
         for j in range(BATCH_SIZE):
             img = Image.fromarray(b_image[j])
             img_2 = (pred[j] * 10)
@@ -61,7 +109,7 @@ with tf.Session() as sess:
             plt.imshow(anno)
             plt.axis('off')
             plt.show()
-
+        '''
 
     coord.request_stop()
     # 其他所有线程关闭后，这一函数才能返回
